d1cca573b87f1d469f51478efc609aed05a3d22d
Gregory <lkorron@yandex.ru>
2021-03-02 14:30:25 +0300

    menu

---
+++ PhysicsModelling/Assets/Scripts/SceneChooser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChooser : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
