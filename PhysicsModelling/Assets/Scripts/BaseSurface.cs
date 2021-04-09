73ec438335c2bf78032d28b91560521f6682afa2
Gregory <lkorron@yandex.ru>
2021-04-09 16:40:13 +0300

    Pendulum added

--- PhysicsModelling/Assets/Scripts/BaseSurface.cs
+++ PhysicsModelling/Assets/Scripts/BaseSurface.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider))]
 public class BaseSurface : MonoBehaviour
 {
     // Class for determing when stopwatch need to be stopped
+   
 }
+
