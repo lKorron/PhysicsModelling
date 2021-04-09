ef0784135ef8959b88ed780bbfd2d7d11dde6272
Gregory <lkorron@yandex.ru>
2021-04-09 16:41:12 +0300

    Pendulum added2

---
+++ PhysicsModelling/Assets/Scripts/Rope.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Rope : MonoBehaviour
+{
+    [SerializeField] private Transform _targetTransform;
+    [SerializeField] private Transform _pendulumTransform;
+    [SerializeField] private Material _ropeMaterial;
+    [SerializeField] private float _width;
+
+    private LineRenderer _lineRenderer;
+
+    private void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        RenderRope(_width);
+    }
+
+    private void RenderRope(float width)
+    {
+        _lineRenderer.material = _ropeMaterial;
+        _lineRenderer.SetPosition(0, _pendulumTransform.position);
+        _lineRenderer.SetPosition(1, _targetTransform.position);
+        _lineRenderer.startWidth = width;
+        _lineRenderer.endWidth = width;
+
+    }
+
+    
+}
